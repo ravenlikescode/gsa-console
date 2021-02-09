e4421c2c8838203257beb5bf21368764bea0121f
Reetik Patel <Reetik2000@yahoo.com>
2021-02-09 14:37:01 -0600

    🎉 Init sidenav

---
+++ src/common/navigation/component/Sidenav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from "./Sidenav.styles.scss";
+
+export function Sidenav() {
+    return (
+        <div className={styles.sidenav}>
+
+        </div>
+    )
+}
+
+export default Sidenav;
