79611043e7967d361f3a9955674efa1d90f28496
Reetik Patel <Reetik2000@yahoo.com>
2021-02-09 14:37:20 -0600

    ➕🎉 Init routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 import './App.scss';
 
@@ -17,21 +22,49 @@ function App() {
     })
     .catch(error => console.log('error', error));
   }, []);
+
+
   return (
-    <div className="App">
-      <ul>
-        {
-          employees.length < 1 ? <h3>No employees found!</h3>
-          : employees.map((emp, id) => {
-            return <li key={id}>
-              <h3>{emp.firstName} {emp.lastName}</h3>
-              <p>{emp.address}, {emp.city}</p>
-            </li>
-          })
-        }
-      </ul>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/transaction">
+          {/* Insert Transaction Page Component */}
+        </Route>
+        <Route path="/products">
+          {/* Insert Products Page Component */}
+        </Route>
+        <Route path="/user">
+          {/* Insert User Page Component */}
+        </Route>
+        <Route path="/report/sales">
+          {/* Insert Sales Report Page Component */}
+        </Route>
+        <Route path="/report/user">
+          {/* Insert User Report Page Component */}
+        </Route>
+
+        <Route path="/">
+          <div className="App">
+            <ul>
+              {
+                employees.length < 1 ? <h3>No employees found!</h3>
+                : employees.map((emp, id) => {
+                  return <li key={id}>
+                    <h3>{emp.firstName} {emp.lastName}</h3>
+                    <p>{emp.address}, {emp.city}</p>
+                  </li>
+                })
+              }
+            </ul>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
+
+
   );
+
+
 }
 
 // function validateUser(id, password)
