065bbd90f8d3b1d1587eaad164ed41e7e36ed43d
Joshua <jad041@uark.edu>
2021-02-17 09:39:56 -0600

    commit on 'main'

--- src/common/navigation/component/Sidenav.jsx
+++ src/common/navigation/component/Sidenav.jsx
@@ -8,28 +8,26 @@ export function Sidenav() {
             <div class = "Sidenav">
                 <div class="Logo" style={{backgroundImage: "url(/img/gsa_logo.png)"}}></div>
                 <div class="Separator"></div>
-                <div class="SidenavBtnContainer">
-                    <a href="http://localhost:3000/" class="SidenavBtn hover">
-                        <ShoppingCart class="Icon noHover"/>
-                        <div class="SidenavBtnText noHover">Start Transaction</div>
-                    </a>
-                    <a href="http://localhost:3000/" class="SidenavBtn">
-                        <Truck class="Icon noHover"/>
-                        <div class="SidenavBtnText noHover">View Products</div>
-                    </a>
-                    <a href="http://localhost:3000/" class="SidenavBtn">
-                        <UserPlus class="Icon noHover"/>
-                        <div class="SidenavBtnText noHover">Create User</div>
-                    </a>
-                    <a href="http://localhost:3000/" class="SidenavBtn">
-                        <DollarSign class="Icon noHover"/>
-                        <div class="SidenavBtnText noHover">Sales Report</div>
-                    </a>
-                    <a href="http://localhost:3000/" class="SidenavBtn">
-                        <Users class="Icon noHover"/>
-                        <div class="SidenavBtnText noHover">Support</div>
-                    </a>
-                </div>
+                <a href="http://localhost:3000/" class="SidenavBtn hover">
+                    <ShoppingCart class="Icon noHover"/>
+                    <div class="SidenavBtnText noHover">Start Transaction</div>
+                </a>
+                <a href="http://localhost:3000/" class="SidenavBtn">
+                    <Truck class="Icon noHover"/>
+                    <div class="SidenavBtnText noHover">View Products</div>
+                </a>
+                <a href="http://localhost:3000/" class="SidenavBtn">
+                    <UserPlus class="Icon noHover"/>
+                    <div class="SidenavBtnText noHover">Create User</div>
+                </a>
+                <a href="http://localhost:3000/" class="SidenavBtn">
+                    <DollarSign class="Icon noHover"/>
+                    <div class="SidenavBtnText noHover">Sales Report</div>
+                </a>
+                <a href="http://localhost:3000/" class="SidenavBtn">
+                    <Users class="Icon noHover"/>
+                    <div class="SidenavBtnText noHover">Support</div>
+                </a>
                 <div class="Separator"></div>
                 <a href="http://localhost:3000/" class="SignOutBtn">
                     <div class="SignOutBtnText">
